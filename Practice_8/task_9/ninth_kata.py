3e7a4a619edda7dcca69ea36051ffa30f9f8e03f
HalyaYakymchuk <galyayakymchuk19@gmail.com>
2026-03-27 19:41:51 +0200

    Add solution for task 9

---
+++ Practice_8/task_9/ninth_kata.py
@@ -0,0 +1,23 @@
+"""Ninth kata"""
+
+class Node:
+    def __init__(self, next=None):
+        self.next = next
+
+def swap_pairs(head):
+
+    fake_node = Node(0)
+    fake_node.next = head
+    prev = fake_node
+
+    while prev.next and prev.next.next:
+        first = prev.next
+        second = prev.next.next
+
+        first.next = second.next
+        second.next = first
+        prev.next = second
+        prev = first
+
+
+    return fake_node.next
