68d94dde37a857e599b9da39458399874a0d8fff
HalyaYakymchuk <galyayakymchuk19@gmail.com>
2026-03-27 19:42:54 +0200

    Add solution for task 11

---
+++ Practice_8/task_11/eleventh_kata.py
@@ -0,0 +1,19 @@
+"""Eleventh kata"""
+
+def loop_size(node):
+    slow = node
+    fast = node
+
+    while True:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+
+    count = 1
+    slow = slow.next
+    while slow != fast:
+        slow = slow.next
+        count += 1
+
+    return count
